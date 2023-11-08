455a2a17dacf89b6572c0f7babcbcb296289a2b8
mcrkgus <mcrkgus@naver.com>
2023-11-09 00:23:25 +0900

    [Feat] CollectionView 코드 합치기

--- SOPT33_assignment2/SOPT33_assignment2/DayWeatherList/ResultViewController.swift
+++ SOPT33_assignment2/SOPT33_assignment2/DayWeatherList/ResultViewController.swift
@@ -5,49 +5,50 @@
 //  Created by Gahyun Kim on 2023/10/16.
 //
 import UIKit
+import SnapKit
 
 class ResultViewController: UIViewController {
     private var contentView = UIImageView()
-
+    
     private var backgroundView : UIImageView = {
         let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "background")
         return imageView
     }()
-
+    
     private let bottomdivideView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.white.withAlphaComponent(0.1)
         return view
     }()
-
+    
     //하단
     private var mapImage: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "map")
         imageView.tintColor = UIColor(named: "yellow")
         return imageView
     }()
-
+    
     private var currentImage: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "current")
         return imageView
     }()
-
+    
     private var dotImage: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "other")
         return imageView
     }()
-
+    
     private lazy var menuButton : UIButton = {
         let button  = UIButton()
         button.setImage(UIImage(named: "pre"), for: .normal)
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         return button
     }()
-
+    
     private var bottomStackView : UIStackView = {
         let stackview = UIStackView()
         stackview.axis = .horizontal
@@ -56,87 +57,67 @@ class ResultViewController: UIViewController {
         stackview.spacing = 10
         return stackview
     }()
-
-
+    
+    private let detailCollecitonView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 30) , height: (UIScreen.main.bounds.height - 6) / 3)
+        flowLayout.minimumLineSpacing = 5
+        flowLayout.minimumInteritemSpacing = 5
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         setLayout()
         bottomLayout()
         setcollectionLayout()
-        setCollectionViewLayout()
-        //setCollectionViewConfig()
-        
+
         self.navigationController?.navigationBar.isHidden = false
         
-        self.collectionView.register(TopCollectionViewCell.self, forCellWithReuseIdentifier: TopCollectionViewCell.identifier)
-
-        self.collectionView.register(TimeWeatherCollectionViewCell.self, forCellWithReuseIdentifier: TimeWeatherCollectionViewCell.identifier)
+        self.detailCollecitonView.register(TopCollectionViewCell.self, forCellWithReuseIdentifier: TopCollectionViewCell.identifier)
         
-        self.collectionView.register(DayWeatherCollectionViewCell.self, forCellWithReuseIdentifier: DayWeatherCollectionViewCell.identifier)
+        self.detailCollecitonView.register(TimeWeatherCollectionViewCell.self, forCellWithReuseIdentifier: TimeWeatherCollectionViewCell.identifier)
         
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
+        self.detailCollecitonView.register(DayWeatherCollectionViewCell.self, forCellWithReuseIdentifier: DayWeatherCollectionViewCell.identifier)
+        
+        self.detailCollecitonView.delegate = self
+        self.detailCollecitonView.dataSource = self
         
         
     }
-
+    
     @objc func buttonPressed() {
         print("BUTTON PRESSED")
         navigationController?.popViewController(animated: true)
     }
-
+    
     //기본 레이아웃
     private func setLayout() {
         view.addSubview(backgroundView)
         view.addSubview(contentView)
         contentView.backgroundColor = .clear
-
+        
         backgroundView.snp.makeConstraints {
             $0.centerX.centerY.width.height.equalToSuperview()
         }
-
-
+        
+        
         contentView.snp.makeConstraints {
             $0.top.bottom.leading.trailing.width.equalTo(view)
             $0.height.greaterThanOrEqualTo(view).priority(.high) //?
             
         }
     }
-
+    
+    
     private func setcollectionLayout() {
-        self.view.addSubview(collectionView)
-        collectionView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(20)
-            $0.height.equalTo(600)
-            $0.bottom.equalTo(contentView)
-            $0.leading.trailing.equalTo(contentView).inset(20)
+        self.view.addSubview(detailCollecitonView)
+        detailCollecitonView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(bottomdivideView.snp.top)
         }
-    }
-    
-    private func setCollectionViewLayout() {
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 30) , height: (UIScreen.main.bounds.width - 6) / 10)
-        flowLayout.minimumLineSpacing = 5
-        flowLayout.minimumInteritemSpacing = 5
-        self.collectionView.setCollectionViewLayout(flowLayout, animated: false)
-    }
-
-    private func setCollectionViewConfig() {
-        self.collectionView.register(DayWeatherCollectionViewCell.self,
-                                         forCellWithReuseIdentifier: DayWeatherCollectionViewCell.identifier)
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-    }
-    
-    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
-        $0.layer.backgroundColor = UIColor(red:1, green:1, blue:1, alpha:0).cgColor
-        //$0.layer.backgroundColor = UIColor(red: 0.175, green: 0.201, blue: 0.249, alpha: 0.2).cgColor
-        //$0.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.25).cgColor
-        //$0.layer.cornerRadius = 20
-        //$0.layer.borderWidth = 0.5
-
     }
     
     
@@ -147,31 +128,31 @@ class ResultViewController: UIViewController {
         bottomStackView.addArrangeSubViews(currentImage, dotImage)
         view.addSubview(mapImage)
         view.addSubview(menuButton)
-
+        
         [bottomdivideView, bottomStackView, mapImage, currentImage, dotImage, menuButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
-
-
+        
+        
         bottomdivideView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(UIScreen.main.bounds.height-100)
             $0.height.equalTo(0.5)
             $0.width.equalTo(UIScreen.main.bounds.width)
         }
-
+        
         
         NSLayoutConstraint.activate([
             bottomStackView.topAnchor.constraint(equalTo: bottomdivideView.bottomAnchor, constant: 10),
             bottomStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
+            
             mapImage.topAnchor.constraint(equalTo: bottomdivideView.bottomAnchor, constant: 2),
             mapImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-
+            
             menuButton.topAnchor.constraint(equalTo: bottomdivideView.bottomAnchor, constant: 2),
             menuButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
-
+    
 }
 
 extension UIStackView {
@@ -213,7 +194,7 @@ extension ResultViewController: UICollectionViewDataSource {
             return 0
         }
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if indexPath.section == 0 {
             guard let item = collectionView.dequeueReusableCell(withReuseIdentifier:  TopCollectionViewCell.identifier,for: indexPath) as? TopCollectionViewCell else {return UICollectionViewCell()}
@@ -223,18 +204,16 @@ extension ResultViewController: UICollectionViewDataSource {
             guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: TimeWeatherCollectionViewCell.identifier,for: indexPath) as? TimeWeatherCollectionViewCell else {return UICollectionViewCell()}
             item.bindData(data: TimeweatherList[indexPath.row])
             return item
-//            item = collectionView.dequeueReusableCell(withReuseIdentifier: TimeWeatherCollectionViewCell.identifier, for: indexPath) as!
-//            TimeWeatherCollectionViewCell
         }
         else {
             guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: DayWeatherCollectionViewCell.identifier,for: indexPath) as? DayWeatherCollectionViewCell else {return UICollectionViewCell()}
             item.bindData(data: DayweatherList[indexPath.row])
             return item
-            //item = collectionView.dequeueReusableCell(withReuseIdentifier: DayWeatherCollectionViewCell.identifier, for: indexPath) as! DayWeatherCollectionViewCell
+            
         }
-//        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: DayWeatherCollectionViewCell.identifier,
-//                                                            for: indexPath) as? DayWeatherCollectionViewCell else {return UICollectionViewCell()}
+        //        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: DayWeatherCollectionViewCell.identifier,
+        //                                                            for: indexPath) as? DayWeatherCollectionViewCell else {return UICollectionViewCell()}
         //item.bindData(data: DayweatherList[indexPath.row])
-       
+        
     }
 }
--- SOPT33_assignment2/SOPT33_assignment2/DayWeatherList/TimeWeatherCollectionViewCell.swift
+++ SOPT33_assignment2/SOPT33_assignment2/DayWeatherList/TimeWeatherCollectionViewCell.swift
@@ -18,7 +18,9 @@ class TimeWeatherCollectionViewCell: UICollectionViewCell {
         stackview.distribution = .equalCentering
         return stackview
     }()
+   
     
+    //header로 뺄 거
     private var descriptLabel : UILabel = {
         let label = UILabel()
         label.text = "08:00~09:00에 강우 상태가, 18:00에 한\n때 흐린 상태가 예상됩니다. "
@@ -43,6 +45,7 @@ class TimeWeatherCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.setLayout()
+    
         self.layer.cornerRadius = 20
         self.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.2).cgColor
         self.layer.borderWidth = 0.5
@@ -55,51 +58,106 @@ class TimeWeatherCollectionViewCell: UICollectionViewCell {
     
     
     private func setLayout() {
-//        contentView.addSubview(verticalStackView)
-//
-//        [timeLabel, weatherImage, tempLabel].forEach {
-//            self.verticalStackView.addArrangedSubview($0)
-//        }
-//
-        
+        contentView.addSubview(verticalStackView)
+
         [timeLabel, weatherImage, tempLabel].forEach {
-            self.contentView.addSubview($0)
+            self.verticalStackView.addArrangedSubview($0)
+        }
+        
+        verticalStackView.snp.makeConstraints {
+            $0.top.equalTo(contentView.snp.top).inset(5)
+            $0.leading.equalTo(contentView.snp.leading).inset(5)
         }
 
-//
-//        verticalStackView.snp.makeConstraints {
-//            $0.top.equalTo(contentView.snp.top).inset(5)
-//            $0.leading.equalTo(contentView.snp.leading).inset(5)
-//
+        
+        
+//        [timeLabel, weatherImage, tempLabel].forEach {
+//            self.contentView.addSubview($0)
+//        }
+
+
+        
+//        timeLabel.snp.makeConstraints {
+//            $0.top.equalTo(contentView.snp.top).inset(20)
+//            $0.leading.equalTo(contentView.snp.leading).offset(20)
 //        }
 //
-        
-
-        timeLabel.snp.makeConstraints {
-            $0.top.equalTo(contentView.snp.top).inset(20)
-            $0.leading.equalTo(contentView.snp.leading).offset(20)
-//            $0.leading.equalToSuperview().inset(10)
-        }
-
-        weatherImage.snp.makeConstraints {
-            $0.top.equalTo(timeLabel.snp.bottom).offset(10)
-            $0.leading.equalTo(timeLabel.snp.leading)
-        }
-
-        tempLabel.snp.makeConstraints {
-            $0.top.equalTo(weatherImage.snp.bottom).offset(10)
-            $0.leading.equalTo(weatherImage.snp.leading)
-
-        }
+//        weatherImage.snp.makeConstraints {
+//            $0.top.equalTo(timeLabel.snp.bottom).offset(10)
+//            $0.leading.equalTo(timeLabel.snp.leading)
+//        }
+//
+//        tempLabel.snp.makeConstraints {
+//            $0.top.equalTo(weatherImage.snp.bottom).offset(10)
+//            $0.leading.equalTo(weatherImage.snp.leading)
+//
+//        }
         
     }
+    
+
+    
+
+
+    
     
     func bindData(data: TimeWeatherListData) {
         self.timeLabel.text = data.time
         self.weatherImage.image = UIImage(systemName: data.weatherImage)?.withRenderingMode(.alwaysOriginal)
         self.tempLabel.text = data.temperature
+        //setStack(time: data.time, img: data.weatherImage, temperature: data.temperature)
+
+    }
+    
+
+    func setStack(time: String, img: String, temperature: String) -> UIStackView {
+        let stackview = UIStackView()
+        stackview.axis = .vertical
+        stackview.spacing = 10
+        stackview.distribution = .fillEqually
+        
+        
+        let timeLabel = UILabel()
+        timeLabel.text = time
+        timeLabel.font = .medium(size: 17)
+        timeLabel.textColor = .white
+        timeLabel.textAlignment = .center
+        
+        let weatherimageView = UIImageView()
+        let image = UIImage(named: img)
+        weatherimageView.image = image?.withRenderingMode(.alwaysOriginal)
+        
+        
+        let tempLabel = UILabel()
+        tempLabel.text = temperature
+        tempLabel.font = .medium(size: 22)
+        tempLabel.textColor = .white
+        tempLabel.textAlignment = .center
+        
+        [timeLabel, weatherImage, tempLabel].forEach {
+            stackview.addArrangedSubview($0)
+            $0.snp.makeConstraints {
+                $0.width.equalTo(44)
+            }
+        }
+        return stackview
     }
 }
 
 
-
+//extension TimeWeatherCollectionViewCell: UICollectionViewDelegate {}
+//extension TimeWeatherCollectionViewCell: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return TimeweatherList.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: TimeWeatherCollectionViewCell.identifier,for: indexPath) as? TimeWeatherCollectionViewCell else {return UICollectionViewCell()}
+//        item.bindData(data: TimeweatherList[indexPath.row])
+//        return item
+//    }
+//
+//
+//
+//
+//}

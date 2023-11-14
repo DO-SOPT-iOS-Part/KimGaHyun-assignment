4eacae5d29f601cf81e9440802b9f357038200ed
mcrkgus <mcrkgus@naver.com>
2023-11-14 19:43:56 +0900

    [Fix] OpenWeatherData 수정

--- SOPT33_assignment2/SOPT33_assignment2/WeatherList/OpenWeatherData.swift
+++ SOPT33_assignment2/SOPT33_assignment2/WeatherList/OpenWeatherData.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
+// MARK: - CurrentWeatherDataModel
 struct OpenWeatherData: Codable {
     let coord: Coord
     let weather: [Weather]
@@ -23,20 +24,20 @@ struct OpenWeatherData: Codable {
     let cod: Int
 }
 
-
+// MARK: - Clouds
 struct Clouds: Codable {
     let all: Int
 }
 
-
+// MARK: - Coord
 struct Coord: Codable {
     let lon, lat: Double
 }
 
-
+// MARK: - Main
 struct Main: Codable {
     let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, humidity, seaLevel, grndLevel: Int
+    let pressure, humidity: Int
 
     enum CodingKeys: String, CodingKey {
         case temp
@@ -44,80 +45,24 @@ struct Main: Codable {
         case tempMin = "temp_min"
         case tempMax = "temp_max"
         case pressure, humidity
-        case seaLevel = "sea_level"
-        case grndLevel = "grnd_level"
     }
 }
 
-
+// MARK: - Sys
 struct Sys: Codable {
+    let type, id: Int
     let country: String
     let sunrise, sunset: Int
 }
 
-
+// MARK: - Weather
 struct Weather: Codable {
     let id: Int
     let main, description, icon: String
 }
 
-
+// MARK: - Wind
 struct Wind: Codable {
-    let speed, deg: Int
-    let gust: Double
+    let speed: Double
+    let deg: Int
 }
-
-
-
-
-
-    
-    // Example usage:
-    let json = """
-    {
-        "coord": {
-            "lon": -0.13,
-            "lat": 51.51
-        },
-        "weather": [
-            {
-                "id": 300,
-                "main": "Drizzle",
-                "description": "light intensity drizzle",
-                "icon": "09d"
-            }
-        ],
-        "base": "stations",
-        "main": {
-            "temp": 280.32,
-            "pressure": 1012,
-            "humidity": 81,
-            "temp_min": 279.15,
-            "temp_max": 281.15
-        },
-        "visibility": 10000,
-        "wind": {
-            "speed": 4.1,
-            "deg": 80
-        },
-        "clouds": {
-            "all": 90
-        },
-        "dt": 1485789600,
-        "sys": {
-            "type": 1,
-            "id": 5091,
-            "message": 0.0103,
-            "country": "GB",
-            "sunrise": 1485762037,
-            "sunset": 1485794875
-        },
-        "id": 2643743,
-        "name": "London",
-        "cod": 200
-    }
-"""
-    
-
-
-

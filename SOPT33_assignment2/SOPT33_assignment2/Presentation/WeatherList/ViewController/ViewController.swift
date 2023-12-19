5bbc0d7e961f56cbc55bbd2deccd62d842c40737
mcrkgus <mcrkgus@naver.com>
2023-12-19 15:36:58 +0900

    [Refactor] #13 - MVVM 패턴 ViewController 수정

--- SOPT33_assignment2/SOPT33_assignment2/Presentation/WeatherList/ViewController/ViewController.swift
+++ SOPT33_assignment2/SOPT33_assignment2/Presentation/WeatherList/ViewController/ViewController.swift
@@ -12,141 +12,86 @@ import Then
 
 final class ViewController: UIViewController, UISearchControllerDelegate {
     
+
     // MARK: - set Properties
-    
+
     var searchWeatherListData = weatherList //검색 결과 데이터
     private var weatherListView : WeatherListView?
-    
+    var weatherListViewModel = WeatherListViewModel()
+
     
     // MARK: - Life Cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //api key test
-        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else { return }
-        print(apiKey)
-        
-        Task {
-            await fetchWeatherInfo()
-        }
-        
+
         setWeatherListView()
-     
-        setupNavigation()
-        setDelegate()
+        setNavigation()
         setTableViewConfig()
+        
+        setDelegate()
+        bindViewModel()
     }
-    
-    @objc func buttonPressed() {
-        let resultVC = ResultViewController()  // ResultViewController 초기화
-        navigationController?.pushViewController(resultVC, animated: true) // 화면에 표시
-    }
-   
-    
-    // MARK: - set Navigation
-    
-    func setupNavigation() {
-        self.navigationItem.searchController = weatherListView?.searchController
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        self.navigationItem.title = "날씨"
-        self.navigationItem.hidesSearchBarWhenScrolling = false
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        loadData()
     }
 
     
-    
     // MARK: - set WeatherList View
-    
+
     private func setWeatherListView() {
         weatherListView = WeatherListView(frame: view.bounds)
         if let resultView = weatherListView {
             view.addSubview(resultView)
             resultView.configure()
         }
-        // 검색 
-        weatherListView?.searchController.searchResultsUpdater = self
+        self.weatherListView?.searchController.searchResultsUpdater = self
     }
+    
+    
+    // MARK: - set Navigation
 
-    
-    // MARK: - set Delegate
-    
-    private func setDelegate() {
-        weatherListView?.tableView.delegate = self
-        weatherListView?.tableView.dataSource = self
+    func setNavigation() {
+        self.navigationItem.searchController = weatherListView?.searchController
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "날씨"
+        self.navigationItem.hidesSearchBarWhenScrolling = false
     }
     
     
     // MARK: - set TableView
-    
+
     private func setTableViewConfig() {
         weatherListView?.tableView.register(WeatherListTableViewCell.self,
                                 forCellReuseIdentifier: WeatherListTableViewCell.className)
     }
-    
 
-    // MARK: - Network
 
-    //API에서 가져오는 timezone 현재 시간으로 변경하기
-    func convertTime(timezone: Int) -> String {
-        let timeZone = TimeZone(secondsFromGMT: timezone)
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm"
-        dateFormatter.timeZone = timeZone
-        let currentDate = Date()
-        let formattedTime = dateFormatter.string(from: currentDate)
-        return formattedTime
+    // MARK: - set Delegate
+
+    private func setDelegate() {
+        weatherListView?.tableView.delegate = self
     }
+
     
-    private func fetchWeatherInfo() async {
-            let cityname = ["seoul",
-                            "daegu",
-                            "ulsan",
-                            "chuncheon",
-                            "jeju",
-                            "gwangju",
-                            "suwon",
-                            "iksan",
-                            "busan"]
-            
-            for city in cityname {
-                do {
-                    let currentWeather = try await WeatherService.shared.getWeatherData(cityname: city)
-                    print("type:", type(of: currentWeather.timezone))
-                    let weatherInfo = WeatherListData(location: currentWeather.name,
-                                                      time: convertTime(timezone: currentWeather.timezone),
-                                                      weather: currentWeather.weather[0].main,
-                                                      temperature: Int(currentWeather.main.temp),
-                                                      max: Int(currentWeather.main.tempMax),
-                                                      min: Int(currentWeather.main.tempMin))
-                    weatherList.append(weatherInfo)
-                    searchWeatherListData = weatherList
-                } catch {
-                    print(error)
-                }
-            }
-            weatherListView?.tableView.reloadData()
-        }
+    // MARK: - binding ViewModel
+    
+    private func bindViewModel() {
+        self.weatherListView?.tableView.dataSource = weatherListViewModel
+        self.weatherListView?.tableView.reloadData()
+    }
 }
 
 
-extension ViewController: UITableViewDelegate {}
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchWeatherListData.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: WeatherListTableViewCell.className,
-            for: indexPath) as? WeatherListTableViewCell else {return UITableViewCell()}
-        
-        cell.bindData(data: searchWeatherListData[indexPath.row])
-        return cell
-    }
-
+// Cell 클릭 시 화면 이동
+extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             tableView.deselectRow(at: indexPath, animated: true)
             print("Click Cell Number:" + String(indexPath.row))
-            buttonPressed()
+        let resultVC = ResultViewController()
+        navigationController?.pushViewController(resultVC, animated: true)
     }
 }
 
@@ -161,5 +106,17 @@ extension ViewController: UISearchResultsUpdating {
             searchWeatherListData = weatherList.filter { $0.location.lowercased().contains(text.lowercased()) }
         }
         weatherListView?.tableView.reloadData()
+    }
+}
+
+// 날씨 데이터
+extension ViewController {
+    private func loadData() {
+        Task {
+            let success = await weatherListViewModel.fetchWeatherInfo()
+            if success {
+                self.weatherListView?.tableView.reloadData()
+            }
+        }
     }
 }
